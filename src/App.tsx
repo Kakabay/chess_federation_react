09a6171056eec238dd0bc0ef63f289335c1c7298
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-09-16 17:53:17 +0500

    page bg changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { Outlet } from "react-router-dom";
-import Header from "./components/shared/header";
-import Footer from "./components/shared/footer";
+import { Outlet } from 'react-router-dom';
+import Header from './components/shared/header';
+import Footer from './components/shared/footer';
 
 function App() {
   return (
     <main className="flex flex-col h-screen">
       <Header />
 
-      <div className="flex-auto">
+      <div className="flex-auto bg-PAGE_BG">
         <Outlet />
       </div>
 
