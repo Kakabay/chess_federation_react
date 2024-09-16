29ccabb4125bcf407fea18326f82003ec4578851
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-09-16 17:53:39 +0500

    react router dom changed

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,34 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Home from "./pages/home.tsx";
-import News from "./pages/news.tsx";
+import Home from './pages/home.tsx';
+import News from './pages/news.tsx';
 
-import "./index.css";
-import App from "./App.tsx";
-import About from "./pages/about.tsx";
-import Events from "./pages/events.tsx";
-import Contacts from "./pages/contacts.tsx";
+import './index.css';
+import App from './App.tsx';
+import About from './pages/about.tsx';
+import Events from './pages/events.tsx';
+import Contacts from './pages/contacts.tsx';
+import NewsSingle from './pages/news-single.tsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
-      { path: "", element: <Home /> },
-      { path: "news", element: <News /> },
-      { path: "about", element: <About /> },
-      { path: "events", element: <Events /> },
-      { path: "contacts", element: <Contacts /> },
+      { path: '', element: <Home /> },
+      { path: 'news', element: <News /> },
+      { path: 'about', element: <About /> },
+      { path: 'events', element: <Events /> },
+      { path: 'contacts', element: <Contacts /> },
+      { path: 'news/:pageId', element: <NewsSingle /> },
     ],
   },
 ]);
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StrictMode>,
 );
