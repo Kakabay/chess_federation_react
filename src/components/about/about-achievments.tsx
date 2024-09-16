bf67305141e5b61fe683fcb4e8f30b2cfadad3aa
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-09-16 17:41:40 +0500

    about page done

--- src/components/about/about-achievments.tsx
+++ src/components/about/about-achievments.tsx
@@ -1,21 +1,21 @@
-import clsx from "clsx";
-import Container from "../layout/container";
+import clsx from 'clsx';
+import Container from '../layout/container';
 
 export const achievmentsData = [
   {
-    name: "Проведеных турниров",
+    name: 'Проведеных турниров',
     number: 47,
   },
   {
-    name: "Организованных мероприятий",
+    name: 'Организованных мероприятий',
     number: 47,
   },
   {
-    name: "Выпущеных учеников",
+    name: 'Выпущеных учеников',
     number: 432,
   },
   {
-    name: "Занятых мест",
+    name: 'Занятых мест',
     number: 40,
   },
 ];
@@ -28,14 +28,13 @@ const AboutAchievments = () => {
           <div
             key={i}
             className={clsx(
-              "flex h-full flex-[0_1_350px] even:bg-[#533321] odd:bg-BROWN even:px-10 py-20 flex-col justify-between z-10",
+              'flex h-full flex-[0_1_350px] even:bg-[#533321] odd:bg-BROWN even:px-10 py-20 flex-col justify-between z-10',
               {
-                "pl-10": i === 2,
-              }
-            )}
-          >
+                'pl-10': i === 2,
+              },
+            )}>
             <h4 className="text-[#C5BBB5]">{item.name}</h4>
-            <div className="h1 text-white">{item.number}</div>
+            <div className="h1 !text-white">{item.number}</div>
           </div>
         ))}
       </Container>
--- src/components/about/about-hero.tsx
+++ src/components/about/about-hero.tsx
@@ -1,27 +1,24 @@
-import Container from "../layout/container";
+import Container from '../layout/container';
+import PageTitle from '../shared/page-title';
 
 const AboutHero = () => {
   return (
     <section>
       <Container>
-        <h1 className="h1 text-BROWN font-[bitter] mb-10">Немного о нас</h1>
+        <PageTitle title="Немного о нас" />
+
         <div className="flex items-start leading-[150%] justify-end w-full gap-10 mb-10">
           <p className="w-[400px]">
-            Шахматная федерация - это организация, которая занимается развитием
-            и популяризацией шахмат в стране или регионе.
+            Шахматная федерация - это организация, которая занимается развитием и популяризацией
+            шахмат в стране или регионе.
           </p>
           <p className="w-[400px]">
-            Она проводит различные мероприятия, такие как турниры, чемпионаты,
-            обучающие курсы и семинары, а также участвует в международных
-            соревнованиях.
+            Она проводит различные мероприятия, такие как турниры, чемпионаты, обучающие курсы и
+            семинары, а также участвует в международных соревнованиях.
           </p>
         </div>
         <div className="w-full max-h-[540px]">
-          <img
-            src="/images/about/about.png"
-            alt=""
-            className="w-full h-full object-cover"
-          />
+          <img src="/images/about/about.png" alt="" className="w-full h-full object-cover" />
         </div>
       </Container>
     </section>
