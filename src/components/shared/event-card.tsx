9383a121e9f294965433b639d87b16c580d695eb
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-09-17 18:07:51 +0500

    event card fixed

--- src/components/shared/event-card.tsx
+++ src/components/shared/event-card.tsx
@@ -12,6 +12,7 @@ interface Props {
 }
 
 const EventCard = ({ start, end, className, venue, title, line }: Props) => {
+  console.log(start);
   return (
     <motion.div
       className={cn('flex items-start gap-10 max-w-[952px]', className, {
@@ -25,10 +26,17 @@ const EventCard = ({ start, end, className, venue, title, line }: Props) => {
       whileInView={{ translateY: 0, opacity: 1 }}
       viewport={{ once: true }}
       transition={{ delay: 0.2, duration: 0.6, ease: [0.55, 0, 0.1, 1] }}>
-      <div className="flex flex-col gap-[9px] flex-[1_1_100px] text-DGRAY2 leading-none">
-        <div className="first-letter:!h3">{start}</div>
+      <div className="flex flex-col gap-[9px] flex-[1_1_100px] text-DGRAY2 leading-[120%]">
+        <h3 className="">
+          <span className="h3 text-BLACK">{start.split(' ')[0]} </span>
+          {start.split(' ')[1]}
+        </h3>
         <Separator className="h-[2px] bg-BLACK" />
-        <div className="first-letter:!h3">{end}</div>
+
+        <h3 className="">
+          <span className="h3 text-BLACK">{end.split(' ')[0]} </span>
+          {end.split(' ')[1]}
+        </h3>
       </div>
 
       <div className="flex flex-col gap-5">
