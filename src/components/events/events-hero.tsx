617a59b18daacb4302c6efc06c87a650b1eb7b9e
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-09-17 18:08:00 +0500

    events page hero animated

--- src/components/events/events-hero.tsx
+++ src/components/events/events-hero.tsx
@@ -1,19 +1,56 @@
-import Container from "../layout/container";
+import { motion } from 'framer-motion';
+import Container from '../layout/container';
 
 const EventsHero = () => {
   return (
     <section>
       <Container>
-        <img
-          src="/images/events/1.png"
-          alt=""
-          className="max-h-[600px] max-w-[600px] object-cover mx-auto"
-        />
-        <h1 className="text-[109px] -mt-10 mb-5 font-[bitter] text-BROWN uppercase font-bold leading-[110%]">
-          Предстоящие события
-        </h1>
-        <div className="text-center text-BROWN">
-          Список предстоящих шахматных событий в Туркменистане
+        <div>
+          <div className="h-[600px] w-[600px] mx-auto relative overflow-hidden">
+            <motion.div
+              initial={{
+                height: '100%',
+              }}
+              whileInView={{ height: 0 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.2, duration: 0.6, ease: [0.55, 0, 0.1, 1] }}
+              className="w-full h-full absolute right-0 bottom-0 bg-PAGE_BG z-20"></motion.div>
+            <motion.div
+              initial={{
+                height: 0,
+              }}
+              whileInView={{ height: '100%' }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.2, duration: 0.6, ease: [0.55, 0, 0.1, 1] }}
+              className="w-full h-full absolute right-0 top-0 bg-gradient-to-b from-[#a6785e00] to-[#A6785E] opacity-20 z-10"></motion.div>
+            <motion.img
+              src="/images/events/1.png"
+              alt=""
+              className="w-full h-full object-cover"
+              initial={{
+                scale: 1.5,
+              }}
+              whileInView={{ scale: 1 }}
+              viewport={{ once: true }}
+              transition={{ delay: 0.2, duration: 0.6, ease: [0.55, 0, 0.1, 1] }}
+            />
+          </div>
+          <motion.div
+            initial={{
+              translateY: '25%',
+              opacity: 0,
+            }}
+            whileInView={{ translateY: 0, opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ delay: 0.6, duration: 0.6, ease: [0.55, 0, 0.1, 1] }}
+            className="flex flex-col gap-5 -mt-10 z-50">
+            <h1 className="text-[109px] font-[bitter] text-BROWN uppercase font-bold leading-[110%] z-20">
+              Предстоящие события
+            </h1>
+            <div className="text-center text-BROWN">
+              Список предстоящих шахматных событий в Туркменистане
+            </div>
+          </motion.div>
         </div>
       </Container>
     </section>
