be686b2f5d788d53bf10bb5026c23c71c4c6548c
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-09-28 14:45:59 +0500

    commit

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,8 +1,8 @@
-import EventsHero from "@/components/events/events-hero";
-import EventsMonth from "@/components/events/events-month";
-import { useGetEvents } from "@/lib/hooks/useGetEvents";
-import useScrollToTop from "@/lib/hooks/useScrollToTop";
-import { useZusLang } from "@/zustand/use-zus-lang";
+import EventsHero from '@/components/events/events-hero';
+import EventsMonth from '@/components/events/events-month';
+import { useGetEvents } from '@/lib/hooks/useGetEvents';
+import useScrollToTop from '@/lib/hooks/useScrollToTop';
+import { useZusLang } from '@/zustand/use-zus-lang';
 
 const Events = () => {
   const activeLang = useZusLang().activeLang;
@@ -15,14 +15,15 @@ const Events = () => {
       <EventsHero />
 
       <section className="flex flex-col gap-[72px] md:gap-[120px]">
-        {data?.map((item, i) => (
+        {/* {data?.map((item, i) => (
           <EventsMonth
             key={item.id}
             date={item.header}
             eventsData={item.events}
             isCurrent={i === 0 ? true : false}
           />
-        ))}
+        ))} */}
+        {data && data.map}
       </section>
     </main>
   );
