80c46f57a83d19abe79a4bd69ca7422271508445
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-10-24 15:44:00 +0500

    slider fixed

--- src/components/home/home-partners.tsx
+++ src/components/home/home-partners.tsx
@@ -19,33 +19,22 @@ const HomePartners = () => {
           className="font-[bitter]"
         />
 
-        {data && (
-          <Carousel className="flex items-center">
-            <CarouselContent>
-              {data.map((partner, i) => (
+        <Carousel className="flex items-center" opts={{ skipSnaps: true, dragFree: false }}>
+          <CarouselContent>
+            {data &&
+              data.map((partner, i) => (
                 <CarouselItem
-                  key={partner.id}
-                  className="mr-10 pl-0 basis-[150px] md:basis-[200px]">
-                  <motion.div
-                    initial={{
-                      translateY: '25%',
-                      opacity: 0,
-                    }}
-                    whileInView={{ translateY: 0, opacity: 1 }}
-                    viewport={{ once: true }}
-                    transition={{ delay: i * 0.1, duration: 0.6, ease: [0.55, 0, 0.1, 1] }}
-                    className="w-[200px] h-[100px]">
-                    <img
-                      src={HOSTING + partner.partner}
-                      alt="partner logo"
-                      className="h-full w-full object-cover object-center"
-                    />
-                  </motion.div>
+                  key={i}
+                  className="mr-10 pl-0 basis-[150px] md:basis-[200px]  h-[100px]">
+                  <img
+                    src={HOSTING + partner.partner}
+                    alt={partner.note}
+                    className="h-full w-full object-contain"
+                  />
                 </CarouselItem>
               ))}
-            </CarouselContent>
-          </Carousel>
-        )}
+          </CarouselContent>
+        </Carousel>
       </Container>
     </section>
   );
