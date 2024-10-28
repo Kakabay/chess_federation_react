bc51731dc89b78cf602640f35470b977442f1596
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-10-28 16:16:16 +0500

    build

--- src/components/home/home-rating.tsx
+++ src/components/home/home-rating.tsx
@@ -8,20 +8,26 @@ import { Link } from 'react-router-dom';
 
 const rating = [
   {
-    nameRu: 'Рейтинг Туркменистана среди мужчин',
-    nameTm: 'Erkekleriň arasynda Türkmenistanyň reýtingi',
+    // nameRu: 'Рейтинг Туркменистана среди мужчин',
+    // nameTm: 'Erkekleriň arasynda Türkmenistanyň reýtingi',
+    nameRu: 'Мужчины',
+    nameTm: 'Erkekler',
     img: '/images/home/rating-1.png',
     url: 'https://ratings.fide.com/rankings.phtml?continent=0&country=TKM&rating=standard&gender=&age1=0&age2=0&period=2024-10-01&period2=1',
   },
   {
-    nameRu: 'Рейтинг Туркменистана среди женщин',
-    nameTm: 'Aýallaryň arasynda Türkmenistanyň reýtingi',
+    // nameRu: 'Рейтинг Туркменистана среди женщин',
+    // nameTm: 'Aýallaryň arasynda Türkmenistanyň reýtingi',
+    nameRu: 'Женщины',
+    nameTm: 'Aýal-gyzlar',
     img: '/images/home/rating-2.png',
     url: 'https://ratings.fide.com/rankings.phtml?continent=0&country=TKM&rating=standard&gender=F&age1=0&age2=0&period=2024-10-01&period2=1',
   },
   {
-    nameRu: 'Рейтинг Туркменистана среди юниоров',
-    nameTm: 'Ýetginjekleriň arasynda Türkmenistanyň reýtingi',
+    // nameRu: 'Рейтинг Туркменистана среди юниоров',
+    // nameTm: 'Ýetginjekleriň arasynda Türkmenistanyň reýtingi',
+    nameRu: 'Юниоры',
+    nameTm: 'Ýetginjekler',
     img: '/images/home/rating-3.png',
     url: 'https://ratings.fide.com/rankings.phtml?continent=0&country=TKM&rating=standard&gender=&age1=0&age2=20&period=2024-10-01&period2=1',
   },
@@ -39,12 +45,12 @@ const HomeRating = () => {
           className="font-[bitter]"
         />
 
-        <div className="md:flex hidden items-center gap-10 font-[bitter]">
+        <div className="md:flex hidden items-center w-full gap-10 font-[bitter]">
           {rating.map((item, i) => (
-            <Link target="_blank" to={item.url}>
+            <Link target="_blank" to={item.url} className="w-full">
               <motion.div
                 key={i}
-                className="p-5 border flex items-center gap-5 border-LBROWN rounded-sm"
+                className="p-5 border flex items-center gap-5 border-LBROWN rounded-sm w-full"
                 initial={{
                   translateY: '25%',
                   opacity: 0,
@@ -53,7 +59,7 @@ const HomeRating = () => {
                 viewport={{ once: true }}
                 transition={{ delay: i * 0.1, duration: 0.6, ease: [0.55, 0, 0.1, 1] }}>
                 <img src={item.img} alt="chess-icon" />
-                <h4 className="leading-[150%] text-[20px] font-semibold">
+                <h4 className="leading-[150%] text-[30px] font-semibold">
                   {activeLang.value === 'ru' ? item.nameRu : item.nameTm}
                 </h4>
               </motion.div>
