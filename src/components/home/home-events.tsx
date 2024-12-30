997ae1e09733ebf825f76f20eaa26f95ae457759
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-12-30 19:18:16 +0500

    Refactor HomeEvents component for improved readability and structure; updated import formatting, adjusted responsive classes, and enhanced Carousel item rendering.

--- src/components/home/home-events.tsx
+++ src/components/home/home-events.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import Container from '../layout/container';
-import EventCard from '../shared/event-card';
-import { Separator } from '../ui/separator';
-import SectionHeader from './section-header';
-import { useZusLang } from '@/zustand/use-zus-lang';
-import { Carousel, CarouselContent, CarouselItem } from '../ui/carousel';
-import { Button } from '../ui/button';
-import CalendarMobile from '../shared/calendar-mobile';
-import CalendarDesktop from '../shared/calendar-desktop';
-import { useGetEventsByDate } from '@/lib/hooks/useGetEventsByDate';
-import { useGetCurrentDate } from '@/lib/hooks/useGetCurrentDate';
-import useExtractSectionTitle from '@/lib/hooks/useExtractSectionTitle';
+import { useState } from "react";
+import Container from "../layout/container";
+import EventCard from "../shared/event-card";
+import { Separator } from "../ui/separator";
+import SectionHeader from "./section-header";
+import { useZusLang } from "@/zustand/use-zus-lang";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+import { Button } from "../ui/button";
+import CalendarMobile from "../shared/calendar-mobile";
+import CalendarDesktop from "../shared/calendar-desktop";
+import { useGetEventsByDate } from "@/lib/hooks/useGetEventsByDate";
+import { useGetCurrentDate } from "@/lib/hooks/useGetCurrentDate";
+import useExtractSectionTitle from "@/lib/hooks/useExtractSectionTitle";
 
 const HomeEvents = () => {
   // const activeLang = useZusLang().activeLang;
@@ -18,8 +18,11 @@ const HomeEvents = () => {
   const activeLang = useZusLang().activeLang;
 
   const currentDate = useGetCurrentDate();
-  const { data } = useGetEventsByDate({ date: currentDate, lang: activeLang.value });
-  const sectionTitle = useExtractSectionTitle('future_events_section_title');
+  const { data } = useGetEventsByDate({
+    date: currentDate,
+    lang: activeLang.value,
+  });
+  const sectionTitle = useExtractSectionTitle("future_events_section_title");
 
   return (
     <>
@@ -31,20 +34,21 @@ const HomeEvents = () => {
               title={sectionTitle}
               icon="/images/home/chess-horse.svg"
               link={{
-                text: activeLang.value === 'ru' ? 'все события' : 'hemme çäreler',
-                path: '/events',
+                text:
+                  activeLang.value === "ru" ? "все события" : "hemme çäreler",
+                path: "/events",
               }}
               titleClassName="font-[bitter] leading-none"
             />
           )}
 
-          <div className="lg:flex items-start justify-between hidden">
+          <div className="md:flex items-start justify-between hidden">
             <div className="flex flex-col gap-10 max-w-[700px] xl:max-w-[952px]">
               {data &&
                 data.future_events.map(
                   (item, i) =>
                     i < 2 && (
-                      <div key={i} className="flex flex-col gap-10">
+                      <div key={i} className="flex flex-col gap-10 w-full">
                         <EventCard
                           end={item.end_event_date}
                           name={item.name_of_event}
@@ -55,28 +59,37 @@ const HomeEvents = () => {
                         />
                         {i < 3 - 1 && <Separator />}
                       </div>
-                    ),
+                    )
                 )}
             </div>
 
             <CalendarDesktop />
           </div>
 
-          <Button onClick={() => setCalendar(true)} className="lg:hidden uppercase w-full mb-6">
+          <Button
+            onClick={() => setCalendar(true)}
+            className="md:hidden uppercase w-full mb-6"
+          >
             ОТКРЫТЬ КАЛЕНДАРЬ
           </Button>
 
-          <Carousel className="lg:hidden">
+          <Carousel className="md:hidden flex">
             <CarouselContent>
               {data &&
                 data.future_events.map((item, i) => (
-                  <CarouselItem key={i} className="flex flex-col p-0 basis-[100%]">
-                    end={item.end_event_date}
-                    name={item.name_of_event}
-                    key={item.id}
-                    place={item.place}
-                    start={item.start_event_date}
-                    isCurrent={false} {i < 3 - 1 && <Separator className="lg:block hidden" />}
+                  <CarouselItem
+                    key={i}
+                    className="flex flex-col p-0 basis-[100%]"
+                  >
+                    <EventCard
+                      end={item.end_event_date}
+                      name={item.name_of_event}
+                      key={item.id}
+                      place={item.place}
+                      start={item.start_event_date}
+                      isCurrent={false}
+                    />
+                    {i < 3 - 1 && <Separator className="md:block hidden" />}
                   </CarouselItem>
                 ))}
             </CarouselContent>
