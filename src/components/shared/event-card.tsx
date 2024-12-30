e910e0911251554500188d4548ddc37024900e5f
Kakabay <2kakabayashyrberdyew@gmail.com>
2024-12-30 19:18:24 +0500

    Refactor EventCard component for improved readability; standardized import formatting, adjusted class names for consistency, and enhanced layout structure.

--- src/components/shared/event-card.tsx
+++ src/components/shared/event-card.tsx
@@ -1,8 +1,8 @@
-import { cn } from '@/lib/utils';
-import { Separator } from '../ui/separator';
-import { motion } from 'framer-motion';
-import { useZusLang } from '@/zustand/use-zus-lang';
-import { months } from '@/lib/constants';
+import { cn } from "@/lib/utils";
+import { Separator } from "../ui/separator";
+import { motion } from "framer-motion";
+import { useZusLang } from "@/zustand/use-zus-lang";
+import { months } from "@/lib/constants";
 
 interface Props {
   start: string;
@@ -10,33 +10,46 @@ interface Props {
   name: string;
   place: string;
   className?: string;
-  line?: 'top' | 'bottom' | 'none';
+  line?: "top" | "bottom" | "none";
   isCurrent?: boolean;
 }
 
-const EventCard = ({ start, end, className, place, name, line, isCurrent = false }: Props) => {
+const EventCard = ({
+  start,
+  end,
+  className,
+  place,
+  name,
+  line,
+  isCurrent = false,
+}: Props) => {
   const activeLang = useZusLang().activeLang;
 
   return !isCurrent ? (
     <motion.div
-      className={cn('flex items-start md:gap-10 gap-2 w-full lg:w-[952px]', className, {
-        'border-t-2 pt-[20px] border-LBROWN': line === 'top',
-        'border-b-2 pb-[20px] border-LBROWN': line === 'bottom',
-      })}
+      className={cn(
+        "flex items-start md:gap-10 gap-2 w-full md:max-w-[952px] w-full",
+        className,
+        {
+          "border-t-2 pt-[20px] border-LBROWN": line === "top",
+          "border-b-2 pb-[20px] border-LBROWN": line === "bottom",
+        }
+      )}
       initial={{
-        translateY: '50%',
+        translateY: "50%",
         opacity: 0,
       }}
       whileInView={{ translateY: 0, opacity: 1 }}
       viewport={{ once: true }}
-      transition={{ delay: 0.2, duration: 0.6, ease: [0.55, 0, 0.1, 1] }}>
+      transition={{ delay: 0.2, duration: 0.6, ease: [0.55, 0, 0.1, 1] }}
+    >
       <div className="flex flex-col gap-[9px] text-DGRAY2 leading-[120%]">
         <h3 className="">
-          <span className="h3 text-BLACK">{end && end.slice(8, 10)} </span>{' '}
+          <span className="h3 text-BLACK">{end && end.slice(8, 10)} </span>{" "}
           {end
-            ? activeLang.value === 'tm'
+            ? activeLang.value === "tm"
               ? months.tm[+end.slice(5, 7) - 1]
-              : activeLang.value === 'ru'
+              : activeLang.value === "ru"
               ? months.ru[+end.slice(5, 7) - 1]
               : months.tm[+end.slice(5, 7) - 1]
             : null}
@@ -46,9 +59,9 @@ const EventCard = ({ start, end, className, place, name, line, isCurrent = false
         <h3 className="">
           <span className="h3 text-BLACK">{start && start.slice(8, 10)} </span>
           {start
-            ? activeLang.value === 'tm'
+            ? activeLang.value === "tm"
               ? months.tm[+start.slice(5, 7) - 1]
-              : activeLang.value === 'ru'
+              : activeLang.value === "ru"
               ? months.ru[+start.slice(5, 7) - 1]
               : months.tm[+start.slice(5, 7) - 1]
             : null}
@@ -57,28 +70,33 @@ const EventCard = ({ start, end, className, place, name, line, isCurrent = false
 
       <div className="flex flex-col gap-5">
         <h3 className="h3 font-[bitter]">{name}</h3>
-        <div className="text-DGRAY2 leading-none">{place}</div>
+        <div className="text-DGRAY2 leading-[140%]">{place}</div>
       </div>
     </motion.div>
   ) : (
     <motion.div
-      className={cn('flex items-start gap-10 max-w-[952px] rounded-[24px] text-white', className, {
-        'border-t-2 pt-5 border-white': line === 'top',
-        'border-b-2 pb-5 border-white': line === 'bottom',
-      })}
+      className={cn(
+        "flex items-start gap-10 max-w-[952px] rounded-[24px] text-white",
+        className,
+        {
+          "border-t-2 pt-5 border-white": line === "top",
+          "border-b-2 pb-5 border-white": line === "bottom",
+        }
+      )}
       initial={{
-        translateY: '50%',
+        translateY: "50%",
         opacity: 0,
       }}
       whileInView={{ translateY: 0, opacity: 1 }}
       viewport={{ once: true }}
-      transition={{ delay: 0.2, duration: 0.6, ease: [0.55, 0, 0.1, 1] }}>
+      transition={{ delay: 0.2, duration: 0.6, ease: [0.55, 0, 0.1, 1] }}
+    >
       <div className="flex flex-col gap-[9px] text-white leading-[120%]">
         <h3 className="text-LBROWN2">
-          <span className="h3 text-white">{end.slice(8, 10)} </span>{' '}
-          {activeLang.value === 'tm'
+          <span className="h3 text-white">{end.slice(8, 10)} </span>{" "}
+          {activeLang.value === "tm"
             ? months.tm[+end.slice(5, 7) - 1]
-            : activeLang.value === 'ru'
+            : activeLang.value === "ru"
             ? months.ru[+end.slice(5, 7) - 1]
             : months.tm[+end.slice(5, 7) - 1]}
         </h3>
@@ -86,9 +104,9 @@ const EventCard = ({ start, end, className, place, name, line, isCurrent = false
 
         <h3 className="text-LBROWN2">
           <span className="h3 text-white">{start.slice(8, 10)} </span>
-          {activeLang.value === 'tm'
+          {activeLang.value === "tm"
             ? months.tm[+start.slice(5, 7) - 1]
-            : activeLang.value === 'ru'
+            : activeLang.value === "ru"
             ? months.ru[+start.slice(5, 7) - 1]
             : months.tm[+start.slice(5, 7) - 1]}
         </h3>
@@ -96,7 +114,7 @@ const EventCard = ({ start, end, className, place, name, line, isCurrent = false
 
       <div className="flex flex-col gap-5">
         <h3 className="h3 font-[bitter]">{name}</h3>
-        <div className="text-LBROWN2 leading-none">{place}</div>
+        <div className="text-LBROWN2 leading-[120%]">{place}</div>
       </div>
     </motion.div>
   );
