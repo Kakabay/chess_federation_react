98d5ebffa852484d2f7bbd6a74abf6a7dc6853b5
Batyr <awbatyr@gmail.com>
2025-02-20 14:48:34 +0500

    removed className

--- src/pages/news-single.tsx
+++ src/pages/news-single.tsx
@@ -1,23 +1,29 @@
-import NewsCard from '@/components/shared/news-card';
-import SectionHeader from '@/components/home/section-header';
-import Container from '@/components/layout/container';
-import useScrollToTop from '@/lib/hooks/useScrollToTop';
-import { useParams } from 'react-router-dom';
-import { useGetSingleNews } from '@/lib/hooks/useGetSingleNews';
-import { useZusLang } from '@/zustand/use-zus-lang';
-import { useGetNews } from '@/lib/hooks/useGetNews';
+import NewsCard from "@/components/shared/news-card";
+import SectionHeader from "@/components/home/section-header";
+import Container from "@/components/layout/container";
+import useScrollToTop from "@/lib/hooks/useScrollToTop";
+import { useParams } from "react-router-dom";
+import { useGetSingleNews } from "@/lib/hooks/useGetSingleNews";
+import { useZusLang } from "@/zustand/use-zus-lang";
+import { useGetNews } from "@/lib/hooks/useGetNews";
 
-import { array } from 'zod';
-import { cn } from '@/lib/utils';
-import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
-import useExtractSectionTitle from '@/lib/hooks/useExtractSectionTitle';
+import { array } from "zod";
+import { cn } from "@/lib/utils";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import useExtractSectionTitle from "@/lib/hooks/useExtractSectionTitle";
 
 const NewsSingle = () => {
   let { pageId } = useParams();
   const activeLang = useZusLang().activeLang;
 
   useScrollToTop(pageId);
-  const newsSectionTitle = useExtractSectionTitle('news_single_page_other_news_section_title');
+  const newsSectionTitle = useExtractSectionTitle(
+    "news_single_page_other_news_section_title"
+  );
 
   const { data: singleNewsData } = useGetSingleNews({
     pageId,
@@ -27,7 +33,7 @@ const NewsSingle = () => {
     lang: activeLang.value,
     per_page: 4,
     page: 1,
-    sort: 'asc',
+    sort: "asc",
   });
 
   return (
@@ -35,7 +41,9 @@ const NewsSingle = () => {
       <Container className="flex flex-col gap-[72px] md:gap-[200px]">
         <section className="flex flex-col gap-6 md:gap-10 items-center">
           <h1 className="h1 text-center">{singleNewsData?.data.title}</h1>
-          <div className="leading-none text-GRAY1">{singleNewsData?.data.published_at}</div>
+          <div className="leading-none text-GRAY1">
+            {singleNewsData?.data.published_at}
+          </div>
           {singleNewsData &&
             singleNewsData.data.featured_images &&
             singleNewsData.data.featured_images[0].path && (
@@ -50,7 +58,9 @@ const NewsSingle = () => {
 
           <div
             className="w-full max-w-[800px] mx-auto flex font-medium md:font-normal flex-col gap-6 md:gap-10 font-[bitter] text-[14px] md:text-[20px] leading-[150%]"
-            dangerouslySetInnerHTML={singleNewsData && { __html: singleNewsData.data.content_html }}
+            dangerouslySetInnerHTML={
+              singleNewsData && { __html: singleNewsData.data.content_html }
+            }
           />
         </section>
         {newsData && (
@@ -66,7 +76,6 @@ const NewsSingle = () => {
                   title={news.title}
                   img={news.featured_images[0].path}
                   titleClassName="h4"
-                  className=""
                   type="small"
                 />
               ))}
@@ -79,8 +88,9 @@ const NewsSingle = () => {
                     <CarouselItem
                       key={i}
                       className={cn({
-                        'mr-4 md:mr-6': i + 1 !== array.length,
-                      })}>
+                        "mr-4 md:mr-6": i + 1 !== array.length,
+                      })}
+                    >
                       <NewsCard
                         key={news.id}
                         id={news.id}
